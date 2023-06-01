8eebc6e803f36782d9887d624adae1b303c232eb
Harith Rathish <harith1996@gmail.com>
2023-06-01 16:12:17 +0200

    Added file uploaders for original and private data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import scagnostics from "./lib/scagnostics.min.js";
+import FileUploadSingle from "./components/FileUploadSingle";
+import { parse } from "papaparse";
+import BinnedData from "./classes/BinnedData";
 
+/* eslint-disable-next-line no-restricted-globals */
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [originalData, setOriginalData] = React.useState<any>(null);
+	const [privBinnedData, setPrivBinnedData] = React.useState<any>(null);
+	const handleOriginalData = (data: File | undefined) => {
+		data!.text().then((data) => {
+			setOriginalData(parse(data, { header: true }).data);
+		});
+	};
+	const handlePrivBinnedData = (data: File | undefined) => {
+		data!.text().then((data) => {
+			setPrivBinnedData(parse(data, { header: false }).data);
+		});
+	};
+  useEffect(() => {
+    if (originalData && privBinnedData) {
+      console.log(originalData);
+      console.log(new scagnostics(originalData));
+      const binned = new BinnedData(privBinnedData);
+      console.log(binned);
+    }
+  }, [originalData, privBinnedData]);
+	return (
+		<div className="App">
+			<div>
+				Upload Original Unbinned Data (.csv)
+				<FileUploadSingle
+					fileExtension=".csv"
+					onUpload={handleOriginalData}
+				></FileUploadSingle>
+			</div>
+			<div>
+				Upload Private Binned Data (.csv)
+				<FileUploadSingle
+					fileExtension=".csv"
+					onUpload={handlePrivBinnedData}
+				></FileUploadSingle>
+			</div>
+		</div>
+	);
 }
 
 export default App;
