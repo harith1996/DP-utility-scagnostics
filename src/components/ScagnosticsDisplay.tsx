24d286e9cef0fce8dbd3ce2e383b355465b0fc3a
Harith Rathish <harith1996@gmail.com>
2023-06-06 15:53:19 +0200

    Display scagnostics in table

--- src/components/ScagnosticsDisplay.tsx
+++ src/components/ScagnosticsDisplay.tsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import scagnostics from '../lib/scagnostics'
+import React from "react";
+import scagnostics from "../lib/scagnostics";
 
-const scores = ['clumpyScore', 'convexScore', 'outlyingScore', 'outlyingUpperBoundScore', 'skewedScore', 'sparseScore', 'striatedScore', 'monotonicScore', 'histogramScore', 'multiModalScore', 'overlappingScore', 'clusteredScore', 'clumpyScore', 'convexScore', 'skinnyScore', 'stringyScore', 'stringyScore', 'monotonicScore', 'histogramScore', 'outlyingScore', 'outlyingUpperBoundScore', 'sparseScore', 'skewedScore']
+const scores = [
+	"clumpyScore",
+	"convexScore",
+	"outlyingScore",
+	"outlyingUpperBound",
+	"skinnyScore",
+	"sparseScore",
+	"striatedScore",
+	"monotonicScore",
+	"stringyScore",
+];
 
 export default function ScagnosticsDisplay(props: any) {
-  return (
-    <div>
-        {scores.map((score) => {
-            if(props.scagnostics.hasOwnProperty(score))
-                return (
-                    // make a table with the scagnostics scores
-                    <div>
-                        <table>
-                            <tr>
-                                <td>{score}</td>
-                                <td>{props.scagnostics[score].toFixed(5)}</td>
-                            </tr>
-                        </table>
-                    </div>
-                )
-            return ""
-        })}
-        </div>
-  )
+	return (
+		<div>
+			<div>
+                <h4>Scagnostics</h4>
+				<table>
+					{scores.map((score) => {
+						if (props.scagnostics.hasOwnProperty(score))
+							return (
+								// make a table with the scagnostics scores
+
+								<tr key={score}>
+									<td>{score}</td>
+									<td>
+										{props.scagnostics[score].toFixed(5)}
+									</td>
+								</tr>
+							);
+						return "";
+					})}
+				</table>
+			</div>
+		</div>
+	);
 }
