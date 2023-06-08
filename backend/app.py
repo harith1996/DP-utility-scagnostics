c44c3b3309359d4e46f0561d29faf6440b121103
Harith Rathish <harith1996@gmail.com>
2023-06-08 11:24:38 +0200

    Added backend

--- backend/app.py
+++ backend/app.py
@@ -4,22 +4,59 @@ import csv
 
 app = Flask(__name__)
 
-@app.route('/datasets/original', methods=['GET'])
-def list_original_data():
-    directory = './datasets/original'  # Specify the directory you want to list files from
-    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    return jsonify(files)
+ROOT_PATH = './backend/'
 
-@app.route('/datasets/private', methods=['GET'])
-def list_private_data():
-    directory = './datasets/private'
+filenames = {'original': [], 'private': []}
+#get names of all original datasets
+for dataType in ['original', 'private']:
+    directory = ROOT_PATH +  'datasets/' + dataType + '/'  # Specify the directory you want to list files from
     files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    return jsonify(files)
+    filenames[dataType] = files
 
-@app.route('/getDataset', methods=['GET'])
-def get_data():
+unique = {
+    'datasetNames': set(),
+    'features': set(),
+    'algorithms': set(),
+    'epsilons': set(),
+    'numBins': set()
+}
+
+#parse the filenames as <datasetName>_<feature>_<algorithm>_<epsilon>_<numBins>.csv
+#return a dictionary with unique values for each feature
+def parse_private_filename(filename):
+    filename = filename.split('.csv')[0]
+    filename = filename.split('_')
+    #Push unique values to the lists
+    unique['datasetNames'].add(filename[0])
+    unique['features'].add(filename[1])
+    unique['algorithms'].add(filename[2])
+    unique['epsilons'].add(filename[3])
+    unique['numBins'].add(filename[4])
+
+for filename in filenames['private']:
+    parse_private_filename(filename)
+
+#list available attributes
+@app.route('/attributes', methods=['GET'])
+def list_attributes():
+    return jsonify(list(unique.keys()))
+
+#list unqiue values
+@app.route('/attributes/unique/<attribute>', methods=['GET'])
+def list_unique(attribute):
+    return jsonify(list(unique[attribute]))
+
+#list datasets in a directory
+@app.route('/datasets/<dataType>', methods=['GET'])
+def list_original_data(dataType):
+    return jsonify(filenames[dataType])
+
+#get a specific dataset
+@app.route('/datasets/<dataType>/<filename>', methods=['GET'])
+def get_data(dataType, filename):
     data = []
-    with open('data.csv', 'r') as file:
+    path = ROOT_PATH + 'datasets/' + dataType + '/'  + filename
+    with open(path, 'r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             data.append(row)
