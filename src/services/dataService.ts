a6b150f82115164b45ec3d15d8d2a67cd9f4163c
Harith Rathish <harith1996@gmail.com>
2023-06-08 11:25:11 +0200

    Created a dataService for communicating with backend

---
+++ src/services/dataService.ts
@@ -0,0 +1,26 @@
+//specifies the host of the backend server
+const HOST = "http://localhost:5000";
+
+//get list of datasets of specified type
+export const getDatasets = async (type: string) => {
+	const response = await fetch(`${HOST}/datasets/${type}`);
+	return await response.json();
+};
+
+//get list of available attributes
+export const getAttributes = async () => {
+	const response = await fetch(`${HOST}/attributes`);
+	return await response.json();
+};
+
+//get unique values of each attribute
+export const getUniqueValues = async (type: string) => {
+	const response = await fetch(`${HOST}/attributes/unique/${type}`);
+	return await response.json();
+};
+
+//get a specific dataset
+export const getDataset = async (type: string, filename: string) => {
+	const response = await fetch(`${HOST}/datasets/${type}/${filename}`);
+	return await response.json();
+};
