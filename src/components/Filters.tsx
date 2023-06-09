ef674533d825f0719c3a24a12f10365ba3a54db0
Harith Rathish <harith1996@gmail.com>
2023-06-09 17:15:59 +0200

    Added labels for each select

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -26,29 +26,38 @@ export default function Filters(props: SubmitHandler) {
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
+			<label htmlFor="Dataset">Dataset</label>
 			<select {...register("Dataset", { required: true })}>
 				<option value="0">0</option>
 				<option value="2">2</option>
 				<option value="3">3</option>
 				<option value="9">9</option>
 			</select>
+			
+			<label>Features</label>
 			<select {...register("Features", { required: true })}>
 				<option value="distribution">distribution</option>
 				<option value="correlation">correlation</option>
 				<option value="clusters">clusters</option>
 			</select>
+
+			<label>Algorithm</label>
 			<select {...register("Algorithm", { required: true })}>
 				<option value="DAWA">DAWA</option>
 				<option value="AGrid">AGrid</option>
 				<option value="AHP">AHP</option>
 				<option value="Geometric">Geometric</option>
 			</select>
+			
+			<label>Epsilon</label>
 			<select {...register("Epsilon", { required: true })}>
 				<option value="0.5">0.5</option>
 				<option value="0.1">0.1</option>
 				<option value="0.05">0.05</option>
 				<option value="0.01">0.01</option>
 			</select>
+			
+			<label>Number of bins</label>
 			<select {...register("Number of bins", { required: true })}>
 				<option value="32">32x32</option>
 				<option value="64">64x64</option>
