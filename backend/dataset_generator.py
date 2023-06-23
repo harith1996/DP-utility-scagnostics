4c6582b4b464b905348ef09bdc8ed41c436c8099
Harith Rathish <harith1996@gmail.com>
2023-06-23 15:11:15 +0200

    Added data generators for clustered and correlated data

--- backend/dataset_generator.py
+++ backend/dataset_generator.py
@@ -1,17 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
+# generate related variables
+from numpy import mean
+from numpy import std
+from numpy.random import randn
+from numpy.random import seed
+from sklearn.datasets import make_classification
+from sklearn.datasets import make_blobs
+from sklearn.datasets import make_gaussian_quantiles
 
 
-def gen_clusters(n=2):    
-    center1 = (30, 60)
-    center2 = (70, 50)
-    distance = 20
-    x1 = np.random.uniform(center1[0], center1[0] + distance, size=(100,))
-    y1 = np.random.normal(center1[1], distance, size=(100,)) 
+plt.figure()
+def gen_clusters(numPoints=4000,n=2):    
+    plt.subplot(325)
+    plt.title(" blobs", fontsize="small")
+    X1, Y1 = make_blobs(n_samples=[500, 1000, 2500], n_features=2, cluster_std=
+                    1)
+    plt.scatter(X1[:, 0], X1[:, 1], marker="o", c=Y1, s=25, edgecolor="k")
 
-    x2 = np.random.uniform(center2[0], center2[0] + distance, size=(100,))
-    y2 = np.random.normal(center2[1], distance, size=(100,)) 
 
-    plt.scatter(x1, y1)
-    plt.scatter(x2, y2)
-    plt.show()
+def gen_correlation(sample_size=4000,slope=10, intercept=50):
+    
+    plt.subplot(326)
+    # seed random number generator
+    seed(1)
+    # prepare data
+    data1 = 20 * randn(1000) + 100
+    data2 = data1 + (slope * randn(1000) + intercept)
+    # summarize
+    print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
+    print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
+    # plot
+    plt.scatter(data1, data2)
+
+gen_correlation(slope = 10, intercept = 50)
+gen_clusters(n=4)
+
+plt.show()
